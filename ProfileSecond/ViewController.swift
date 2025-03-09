e336aa4b0f2a746f1cb4a74c1d5e571b2cc5e9ab
kateyatsk <klaettoe2023@gmail.com>
2025-03-09 21:48:31 +0300

    Initial Commit

---
+++ ProfileSecond/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProfileSecond
+//
+//  Created by Екатерина Яцкевич on 9.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
